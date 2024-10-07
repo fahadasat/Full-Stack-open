166112895fd709765954c5b5c48e8558c66328d0
fahadasat <asatfahad@gmail.com>
2024-10-07 17:32:50 -0600

    1.d_complex_state having the state be an object

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,62 @@
 import {useState} from 'react'
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
+    const [counts, setCounts] = new useState({
+        left: 0,
+        right: 0
+    })
 
-    const Display = ({counter}) => <div>{counter}</div>
-
-    const Button = ({onButtonClick, buttonTitle}) => <button onClick={onButtonClick}> {buttonTitle} </button>
-
-    const increaseCounter = () => {
-        setCounter(counter + 1)
-        console.log(counter + 1)
+    const incLeftCount = () => {
+        setCounts({
+            ...counts,
+            left: counts.left + 1
+        })
     }
 
-    const decreaseCounter = () => {
-        setCounter(counter - 1)
-        console.log(counter - 1)
+    const incRightCount = () => {
+        setCounts({
+            ...counts,
+            right: counts.right + 1
+        })
     }
 
-    const resetCounter = () => {
-        setCounter(0)
-        console.log(0)
-    }
+    // const [counter, setCounter] = useState(0)
+    //
+    // const Display = ({counter}) => <div>{counter}</div>
+    //
+    // const Button = ({onButtonClick, buttonTitle}) => <button onClick={onButtonClick}> {buttonTitle} </button>
+    //
+    // const increaseCounter = () => {
+    //     setCounter(counter + 1)
+    //     console.log(counter + 1)
+    // }
+    //
+    // const decreaseCounter = () => {
+    //     setCounter(counter - 1)
+    //     console.log(counter - 1)
+    // }
+    //
+    // const resetCounter = () => {
+    //     setCounter(0)
+    //     console.log(0)
+    // }
 
     return (
         <div>
-            <Display counter={counter}> </Display>
-            <br/>
-            <Button onButtonClick={increaseCounter} buttonTitle='Add'/>
-            <Button onButtonClick={decreaseCounter} buttonTitle='Subtract'/>
-            <Button onButtonClick={resetCounter} buttonTitle='Reset'/>
+            {counts.left}
+            <button onClick={() => incLeftCount()}>
+                left
+            </button>
+            <button onClick={() => incRightCount()}>
+                right
+            </button>
+            {counts.right}
+
+            {/*<Display counter={counter}> </Display>*/}
+            {/*<br/>*/}
+            {/*<Button onButtonClick={increaseCounter} buttonTitle='Add'/>*/}
+            {/*<Button onButtonClick={decreaseCounter} buttonTitle='Subtract'/>*/}
+            {/*<Button onButtonClick={resetCounter} buttonTitle='Reset'/>*/}
         </div>
 
     )
